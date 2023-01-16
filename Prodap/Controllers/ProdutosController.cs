d6ba8542696a73682016a75bbcb1d44a52b15fdc
douglas amaral fernandes <douglasafernandes@yahoo.com.br>
2023-01-16 15:39:16 -0300

    1601 Validando se produto ja existe antes de cadastrar

--- Prodap/Controllers/ProdutosController.cs
+++ Prodap/Controllers/ProdutosController.cs
@@ -36,15 +36,20 @@ namespace Prodap.Controllers
                     _repositorio = new ProdutosRepositorio();
                     _repositorioCD = new CentroDistribuicaoRepositorio();
 
-                   var CENTRODISTRIBUICAO = _repositorioCD.ListarCentroDistribuicao().FirstOrDefault(T => T.NOME.ToUpper() == _Prodobj.CENTRODISTRIBUICAO.ToUpper());
-                       
-                   if (_repositorio.AddProdutos(_Prodobj,CENTRODISTRIBUICAO.ID)) 
-                   {
-                       ViewBag.Mensagem = "Produto Cadastrado com sucesso.";                            
-                   }                  
-                   else
+                    var CENTRODISTRIBUICAO = _repositorioCD.ListarCentroDistribuicao().FirstOrDefault(T => T.NOME.ToUpper() == _Prodobj.CENTRODISTRIBUICAO.ToUpper());
+
+                    bool ProdExists = (_repositorio.ListarProdutos().Any(P => P.ID_PRODUTO.ToUpper() == _Prodobj.ID_PRODUTO.ToUpper()));
+
+                    if (!ProdExists)
+                    { 
+                        if (_repositorio.AddProdutos(_Prodobj, CENTRODISTRIBUICAO.ID))
+                        {
+                            ViewBag.Mensagem = "Produto Cadastrado com sucesso.";
+                        };
+                    }
+                    else
                     {
-                        ViewBag.Mensagem = "Centro de Distribuição ja existe.";                    
+                        ViewBag.Mensagem = "Produto ja existe.";
 
                     }
             }
