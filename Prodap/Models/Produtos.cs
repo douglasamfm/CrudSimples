d3e9894ca43b9b0c87a097f23f621fe630e79e71
douglas amaral fernandes <douglasafernandes@yahoo.com.br>
2023-01-16 13:24:33 -0300

    Model Produtos

---
+++ Prodap/Models/Produtos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Prodap.Models
+{
+    public class Produtos
+    {
+        [Display(Name = "ID_PRODUTO")]
+        [Required(ErrorMessage = "Informe o código do produto.")]
+        public string ID_PRODUTO { get; set; }
+
+
+        [Display(Name = "Descrição do Produto:")]
+        [Required(ErrorMessage = "Informe a descrição do Produto.")]
+        public string NOME { get; set; }
+
+
+        [Display(Name = "Quantidade:")]
+        [Required(ErrorMessage = "Informe a Quantidade .")]
+        public int QUANTIDADE { get; set; }
+
+
+        [Display(Name = "Centro de Distribuição:")]
+        [Required(ErrorMessage = "Informe o Centro de Distribuição.")]
+        public int ID_CENTRODISTRIBUICAO { get; set; }
+
+    }
+}
